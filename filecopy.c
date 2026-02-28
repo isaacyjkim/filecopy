9e79da298805259b1b1971462075fcc646484d4c
Isaac Kim <isaacyunkim@gmail.com>
2026-02-28 11:30:07 -0800

    file reading

--- filecopy.c
+++ filecopy.c
@@ -7,6 +7,7 @@
 
 #include <unistd.h>
 #include <stdio.h>
+#include <stdlib.h> 
 #include <fcntl.h>
 #include <sys/types.h>
 #include <sys/wait.h>
@@ -16,10 +17,24 @@
 
 int main(int argc, char *argv[])
 {
-	if (argc != 2) { 
-		print("Only 2 arguments alllowed. source - destination.");
+	if (argc != 3) { 
+		printf("Only 2 arguments alllowed. source - destination.");
 		return 1;
+	}
 
+	FILE* fptr; // pointer 
+
+
+	fptr = fopen(argv[1], "r"); 
+
+	if (fptr == NULL) { 
+		printf("File not found."); 
+		return 1; 
+	}
+
+
+
+	
 
 
 	return 0; 
