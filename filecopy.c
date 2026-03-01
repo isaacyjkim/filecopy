6f83a040417926c35b0c4ca09438585a76880c2f
Isaac Kim <isaacyunkim@gmail.com>
2026-03-01 12:05:52 -0800

    fixed error w/ parent process printing success message on error

--- filecopy.c
+++ filecopy.c
@@ -19,7 +19,7 @@ int main(int argc, char *argv[])
 {
 	// Check for only 3 arguments: name of program, source , destination 
 	if (argc != 3) { 
-		printf("Only 2 arguments alllowed. source - destination.");
+		printf("Only 2 arguments alllowed. source - destination.\n");
 		return 1;
 	}
 
@@ -28,15 +28,23 @@ int main(int argc, char *argv[])
 	int fd[2]; 
 
 	if (pipe(fd) == -1 ) { 
-		printf("Pipe error.");
+		printf("Pipe error.\n");
+		return 1; 
+	}
+
+	// Opening file for reading 
+	FILE *fptr; // pointer 
+	fptr = fopen(argv[1], "r"); 
+
+	// Check if file is found
+	if (fptr == NULL) { 
+		printf("Error: Unable to open source file '%s'.\n",argv[1]); 
 		return 1; 
 	}
 	
-	// Creating pipe and assigning int value to id
-	int id = fork(); 
-
-	char buffer[4096];
-	size_t bytes_read; 
+	int id = fork(); // Creating pipe and assigning int value to id
+	char buffer[4096]; // buffer for holding data between pipe 
+	size_t bytes_read; // store # of bytes read from source file and read end of pipe
 
 	// if id == 0, run child process
 	if (id == 0) { 
@@ -58,21 +66,13 @@ int main(int argc, char *argv[])
 		// Close destination file and read end of file descriptor 
 		fclose(output_file); 
 		close(fd[0]);
+		printf("File successfully copied from %s to %s.\n",argv[1],argv[2]);
 	}
 	else { 
 		// Parent Process
 		// Read from source file and write to write end of the pipe
 		close(fd[0]); // close read end of file descriptor
 		
-		// Opening file for reading 
-		FILE *fptr; // pointer 
-		fptr = fopen(argv[1], "r"); 
-
-		// Check if file is found
-		if (fptr == NULL) { 
-			printf("Error: Unable to open source file '%s'.",argv[1]); 
-			return 1; 
-		}
 
 		// Continue to read from source files until no bytes are read, and 
 		// write onto write end of file descriptor. 
@@ -90,7 +90,6 @@ int main(int argc, char *argv[])
 	}
 
 	
-	printf("File successfully copied from %s to %s.",argv[1],argv[2]);
 
 	return 0; 
 }
